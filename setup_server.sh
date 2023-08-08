879cb260baa6f47f26d409a245937272d5919119
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-08-08 15:01:17 +0200

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -3,8 +3,10 @@
 # Github : https://github.com/Kurama250
 
 apt update && apt upgrade -y
-apt install npm node.js zip -y
+apt install npm node.js git -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
 apt-get install -y nodejs -y
+git clone https://github.com/Kurama250/Stats_server.git
+cd Stats_server/
 npm install discord.js@13 child_process
 npm install pm2 -g
