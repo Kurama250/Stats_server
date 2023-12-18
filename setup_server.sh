6dfdb77fce8900dc6aa6fafa320fe5d6706d683d
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-12-18 19:05:10 +0100

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -4,8 +4,12 @@
 
 apt update && apt upgrade -y
 apt install npm nodejs git -y
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
-apt-get install -y nodejs -y
+if ! command -v node &> /dev/null; then
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
+    apt-get install -y nodejs -y
+else
+    echo "Node.js is already installed. Skipping installation."
+fi
 git clone https://github.com/Kurama250/Stats_server.git
 cd Stats_server/
 npm install discord.js@13 child_process
