7f5fc9d6d99a23b98a017ed373466dee039441d2
Kurama <60360593+Kurama250@users.noreply.github.com>
2024-12-14 21:24:23 +0100

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-# setup_server by Kurama250
-# Github : https://github.com/Kurama250
+# setup.sh by Kurama250
+# Github : https://github.com/Kurama250/
 
 apt update && apt upgrade -y
 apt install npm nodejs git lm-sensors -y
+
 if ! command -v node &> /dev/null; then
-    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-    apt-get install -y nodejs -y
+    echo "Node.js is not installed. Installing..."
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+    apt-get install -y nodejs
 else
     echo "Node.js is already installed. Skipping installation."
 fi
+
 git clone https://github.com/Kurama250/Stats_server.git
 cd Stats_server/
 npm install discord.js@13 child_process os
-npm install pm2 -g
+
+if ! command -v pm2 &> /dev/null; then
+    echo "PM2 is not installed. Installing..."
+    npm install pm2 -g
+else
+    echo "PM2 is already installed. Skipping installation."
+fi
